fa89504a355b1258f2a5574ce4bf3c65681743bd
Conor Doherty <doherty.conor@gmail.com>
2020-10-29 15:57:57 -0700

    water balance interp
    Move water balance code to interpolate
    
    Remove use_cloud_api=True from Initialize
    
    saving
    
    Water balance in own function with basic unit test
    
    Water balance test, bug fixes in water balance code
    
    Load csv of model output from python implementation of model to
    create image collection of ndvi, ref et, and precip. Run water
    balance model and compare ET estimates.
    
    Rearrange WB tests into synthetic and real landsat
    
    reset examples to master

--- openet/sims/tests/test_d_interpolate.py
+++ openet/sims/tests/test_d_interpolate.py
@@ -5,6 +5,9 @@ import pytest
 
 import openet.sims.interpolate as interpolate
 import openet.sims.utils as utils
+from openet.sims.image import Image
+import pandas as pd
+import datetime
 
 
 def scene_coll(variables, et_fraction=0.4, et=5, ndvi=0.6):
@@ -54,7 +57,7 @@ def test_from_scene_et_fraction_daily_values(tol=0.0001):
         scene_coll(['et_fraction', 'ndvi', 'time', 'mask']),
         start_date='2017-07-01', end_date='2017-08-01',
         variables=['et', 'et_reference', 'et_fraction', 'ndvi'],
-        interp_args={'interp_method': 'linear', 'interp_days': 32},
+        interp_args={'interp_method': 'linear', 'interp_days': 32,},
         model_args={'et_reference_source': 'IDAHO_EPSCOR/GRIDMET',
                     'et_reference_band': 'eto',
                     'et_reference_factor': 1.0,
@@ -156,3 +159,161 @@ def test_from_scene_et_fraction_monthly_et_reference_resample(tol=0.0001):
     assert abs(output['et_reference']['2017-07-01'] - 232.3) <= tol
     assert abs(output['et']['2017-07-01'] - (232.3 * 0.4)) <= tol
     assert output['count']['2017-07-01'] == 3
+
+def test_water_balance_landsat(tol=0.001):
+    TEST_POINT = (-120.201, 36.1696)
+    et_reference_source = 'IDAHO_EPSCOR/GRIDMET'
+    et_reference_band = 'eto'
+    start_date = '2018-02-23'
+    end_date = '2018-03-08'
+
+    for ls_collection in [1, 2]:
+        ls_suffix = 'SR' if ls_collection == 1 else 'L2'
+
+        ls8_coll_str = f'LANDSAT/LC08/C0{ls_collection}/T1_{ls_suffix}'
+        ls8 = ee.ImageCollection(ls8_coll_str)\
+                .filterDate(start_date, end_date)\
+                .filterBounds(ee.Geometry.Point(TEST_POINT))
+
+        ls7_coll_str = f'LANDSAT/LE07/C0{ls_collection}/T1_{ls_suffix}'
+        ls7 = ee.ImageCollection(ls7_coll_str)\
+                .filterDate(start_date, end_date)\
+                .filterBounds(ee.Geometry.Point(TEST_POINT))
+
+        ls = ls7.merge(ls8)
+        zero = ls.first().select(['B2']).double().multiply(0)
+
+        def make_et_frac(img):
+            if ls_collection == 1:
+                et_img = Image.from_landsat_c1_sr(img,
+                            et_reference_source=et_reference_source, 
+                            et_reference_band=et_reference_band)\
+                        .calculate(['ndvi', 'et_reference', 'et_fraction', 'et'])
+            else:
+                et_img = Image.from_landsat_c2_sr(img,
+                            et_reference_source=et_reference_source, 
+                            et_reference_band=et_reference_band)\
+                        .calculate(['ndvi', 'et_reference', 'et_fraction', 'et'])
+
+            time = ee.Number(img.get('system:time_start'))
+            et_img = et_img.addBands([zero.add(time).rename('time')])
+            return et_img
+
+        test_imgs = ls.map(make_et_frac)
+        normal_coll = interpolate.from_scene_et_fraction(
+            test_imgs,
+            start_date=start_date,
+            end_date=end_date,
+            variables=['et_reference', 'et_fraction', 'et'],
+            interp_args={'interp_method': 'linear', 'interp_days': 14},
+            model_args={'et_reference_source': 'IDAHO_EPSCOR/GRIDMET',
+                        'et_reference_band': 'eto',
+                        'et_reference_factor': 1.0,
+                        'et_reference_resample': 'nearest'},
+            t_interval='daily')
+
+        wb_coll = interpolate.from_scene_et_fraction(
+            test_imgs,
+            start_date=start_date,
+            end_date=end_date,
+            variables=['et_reference', 'et_fraction', 'ke', 'et', 'ndvi'],
+            interp_args={'interp_method': 'linear', 'interp_days': 14,
+                         'water_balance': True},
+            model_args={'et_reference_source': 'IDAHO_EPSCOR/GRIDMET',
+                        'et_reference_band': 'eto',
+                        'et_reference_factor': 1.0,
+                        'et_reference_resample': 'nearest'},
+            t_interval='daily')
+
+        normal = utils.point_coll_value(normal_coll, TEST_POINT, scale=30)
+        wb = utils.point_coll_value(wb_coll, TEST_POINT, scale=30)
+
+        for date in normal['et'].keys():
+            assert wb['et'][date] >= normal['et'][date]
+
+def test_water_balance_synthetic(tol=0.001):
+    TEST_POINT = (-120.201, 36.1696)
+    et_reference_source = 'IDAHO_EPSCOR/GRIDMET'
+    et_reference_band = 'eto'
+    start_date = '2018-02-10'
+    end_date = '2018-03-14'
+
+    ls8_coll_str = f'LANDSAT/LC08/C01/T1_SR'
+    ls8 = ee.ImageCollection(ls8_coll_str)\
+            .filterDate(start_date, end_date)\
+            .filterBounds(ee.Geometry.Point(TEST_POINT))
+
+
+    first = ls8.first().select(['B2']).double().multiply(0)
+    zero = first
+
+    comp_data = pd.read_csv('openet/sims/tests/ee_wb_valid.csv')
+
+    first_day = comp_data.iloc[0]
+    dt = datetime.datetime(2018, 1, 1) + datetime.timedelta(first_day.doy-1)
+    time = ee.Number(ee.Date.fromYMD(2018, dt.month, dt.day).millis())
+    first_img = zero.set({'system:time_start': time}) \
+                .addBands([zero.add(time).rename('time')]) \
+                .addBands([zero.add(first_day.ndvi_interp).rename('ndvi')]) \
+                .addBands([zero.add(first_day.kc).rename('et_fraction')]) \
+                .addBands([zero.add(first_day.eto).rename('et_reference')]) \
+                .select(['time', 'ndvi', 'et_fraction', 'et_reference'])
+
+    test_imgs = ee.ImageCollection(first_img)
+
+    for i, doy in enumerate(comp_data.doy):
+        if i == 0:
+            continue
+
+        day = comp_data.iloc[i]
+        dt = datetime.datetime(2018, 1, 1) + datetime.timedelta(doy-1)
+        time = ee.Number(ee.Date.fromYMD(2018, dt.month, dt.day).millis())
+        next_img = zero.set({'system:time_start': time}) \
+                    .addBands([zero.add(time).rename('time')]) \
+                    .addBands([zero.add(day.ndvi_interp).rename('ndvi')]) \
+                    .addBands([zero.add(day.kc).rename('et_fraction')]) \
+                    .addBands([zero.add(day.eto).rename('et_reference')]) \
+                    .select(['time', 'ndvi', 'et_fraction', 'et_reference'])
+        test_imgs = test_imgs.merge(ee.ImageCollection(next_img))
+
+    normal_coll = interpolate.from_scene_et_fraction(
+        test_imgs,
+        start_date=start_date,
+        end_date=end_date,
+        variables=['et_reference', 'et_fraction', 'et', 'ndvi'],
+        interp_args={'interp_method': 'linear', 'interp_days': 10},
+        model_args={'et_reference_source': 'IDAHO_EPSCOR/GRIDMET',
+                    'et_reference_band': 'eto',
+                    'et_reference_factor': 0.85,
+                    'et_reference_resample': 'nearest'},
+        t_interval='daily')
+
+    wb_coll = interpolate.from_scene_et_fraction(
+        test_imgs,
+        start_date=start_date,
+        end_date=end_date,
+        variables=['et_reference', 'et_fraction', 'ke', 'et', 'ndvi', 'precip'],
+        interp_args={'interp_method': 'linear', 'interp_days': 10,
+                     'water_balance': True},
+        model_args={'et_reference_source': 'IDAHO_EPSCOR/GRIDMET',
+                    'et_reference_band': 'eto',
+                    'et_reference_factor': 0.85,
+                    'et_reference_resample': 'nearest'},
+        t_interval='daily')
+
+    normal = utils.point_coll_value(normal_coll, TEST_POINT, scale=30)
+    wb = utils.point_coll_value(wb_coll, TEST_POINT, scale=30)
+
+    # sanity check that wb ET >= regular SIMS ET
+    for date in normal['et'].keys():
+        assert wb['et'][date] >= normal['et'][date]
+
+    def get_doy(dt_str):
+        return datetime.datetime.strptime(dt_str, '%Y-%m-%d').timetuple().tm_yday
+
+    wb_df = pd.DataFrame(wb)
+    wb_df = wb_df.reset_index()
+    wb_df['doy'] = wb_df['index'].apply(get_doy)
+
+    for i in range(56, 72):
+        assert abs(wb_df[wb_df.doy==i]['et'].iloc[0] - comp_data[comp_data.doy==i]['etc'].iloc[0]) < tol
